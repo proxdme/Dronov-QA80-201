215c0bd6c46bbf08805063a7ca73e8c0449f79b9
MikhailD <proxdme@mail.ru>
2024-04-03 20:32:36 +0300

    testng

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainAmountLessBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900; // amount меньше границы
+        int expectedRemain = 100; // Ожидаемый остаток
+
+        int actualRemain = cashbackHackService.remain(amount);
+
+        Assert.assertEquals(actualRemain, expectedRemain);
+    }
+
+    @Test
+    public void testRemainAmountEqualBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000; // amount равно границе
+        int expectedRemain = 0; // Ожидаемый остаток
+
+        int actualRemain = cashbackHackService.remain(amount);
+
+        Assert.assertEquals(actualRemain, expectedRemain);
+    }
+
+    @Test
+    public void testRemainAmountHighBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1500; // amount больше границы
+        int expectedRemain = 500; // Ожидаемый остаток
+
+        int actualRemain = cashbackHackService.remain(amount);
+
+        Assert.assertEquals(actualRemain, expectedRemain);
+    }
+}
